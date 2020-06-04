b98456c8562cdc80311ceb0e56888f2e3bd2f8c8
Ed Brett <e.j.a.brett@gmail.com>
2020-06-04 14:26:43 +0200

    add error and 404 pages

---
+++ pages/404/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Layout from 'app/layouts/root';
+import ConfirmationMessage from 'components/confirmation-message';
+
+import './styles.scss';
+
+const NotFoundPage = (props) => (
+  <Layout {...props}>
+    <div className="l-404-page">
+      <div className="row">
+        <div className="column small-12 medium-8 medium-offset-2">
+          <ConfirmationMessage
+            title={props.title}
+            description={props.description}
+            error
+            large
+          />
+        </div>
+      </div>
+    </div>
+  </Layout>
+);
+
+NotFoundPage.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default NotFoundPage;
