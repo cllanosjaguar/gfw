de57d15fc3a7a2c6ca99ce6b656b64a87f36cef5
Ed Brett <e.j.a.brett@gmail.com>
2020-06-04 14:13:09 +0200

    add topics pages

--- app/javascript/pages/topics/components/topics-header/topics-intro/component.jsx
+++ app/javascript/pages/topics/components/topics-header/topics-intro/component.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { track } from 'app/analytics';
 
+import { Media } from 'utils/responsive';
+
 import Button from 'components/ui/button';
 import Icon from 'components/ui/icon';
 
@@ -11,28 +13,27 @@ import './styles.scss';
 
 class Intro extends PureComponent {
   render() {
-    const { intro, className, handleSkipToTools, isDesktop } = this.props;
+    const { intro, className, handleSkipToTools } = this.props;
     const { img1x, img2x, title, text, citation } = intro;
 
     return (
       <div className={cx('c-topics-intro', className)}>
         <div className="row titleRow">
           <div className="column small-12 medium-6 titleCol">
-            {isDesktop &&
-              img1x && (
+            <Media greaterThanOrEqual="md">
               <div className="intro-img">
                 <img
-                  {...img2x && {
+                  {...(img2x && {
                     srcSet: `${img2x} 2x, ${img1x} 1x,`,
-                    src: `${img1x} 1x`
-                  }}
-                  {...!img2x && {
-                    src: img1x
-                  }}
+                    src: `${img1x} 1x`,
+                  })}
+                  {...(!img2x && {
+                    src: img1x,
+                  })}
                   alt={title}
                 />
               </div>
-            )}
+            </Media>
           </div>
           <div className="column small-12 medium-6 titleCol">
             <h1 className="intro-title">{title}</h1>
@@ -44,7 +45,7 @@ class Intro extends PureComponent {
                 rel="noopener noreferrer"
                 onClick={() => {
                   track('topicsCitation', {
-                    label: title
+                    label: title,
                   });
                 }}
               >
@@ -57,17 +58,17 @@ class Intro extends PureComponent {
           <div className="column small-12 medium-6" />
           <div className="column small-12 medium-6">
             <p className="intro-text">{text}</p>
-            {isDesktop && (
+            <Media greaterThanOrEqual="md">
               <Button
                 theme="theme-button-light skip-to-tools"
                 onClick={handleSkipToTools}
               >
                 Related tools
               </Button>
-            )}
+            </Media>
           </div>
         </div>
-        {!isDesktop && (
+        <Media lessThan="md">
           <div className="intro-img">
             <img
               srcSet={`${img1x} 2x, ${img2x} 1x,`}
@@ -75,7 +76,7 @@ class Intro extends PureComponent {
               alt={title}
             />
           </div>
-        )}
+        </Media>
       </div>
     );
   }
@@ -85,7 +86,6 @@ Intro.propTypes = {
   intro: PropTypes.object,
   className: PropTypes.string,
   handleSkipToTools: PropTypes.func,
-  isDesktop: PropTypes.bool
 };
 
 export default Intro;
