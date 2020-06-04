4f05f7bdfa90661850cba33d66952a1f68b8b1f5
Ed Brett <e.j.a.brett@gmail.com>
2020-06-04 13:10:49 +0200

    privacy and terms pages

---
+++ pages/privacy-policy.js
@@ -0,0 +1,8 @@
+import Layout from 'app/layouts/root';
+import PrivacyPolicy from 'pages/privacy';
+
+export default (props) => (
+  <Layout {...props}>
+    <PrivacyPolicy />
+  </Layout>
+);
---
+++ pages/terms.js
@@ -0,0 +1,8 @@
+import Layout from 'app/layouts/root';
+import Terms from 'pages/terms';
+
+export default (props) => (
+  <Layout {...props}>
+    <Terms />
+  </Layout>
+);
